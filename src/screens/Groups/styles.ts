be9e98ed5c94c78fcf60e03723d8e1fb68721967
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-06-02 11:45:58 -0300

    feat: create Groups screen

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View(({ theme }) => ({
+  fontSize: 10,
+  backgroundColor: theme.COLORS.GRAY_600,
+  flex: 1,
+}));
+
+export const Text = styled.Text(({ theme }) => ({
+  fontSize: 48,
+  color: theme.COLORS.WHITE,
+}));
