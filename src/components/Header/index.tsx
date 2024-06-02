a530f010f2e496a7f38765889d4fec57fd00a33c
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-06-02 12:14:57 -0300

    feat: setup phosphor icon and rn-svg libraries & use caret icon as back icon for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import { Container, Logo } from "./styles";
+import { Container, Logo, BackButton, BackIcon } from "./styles";
 
 import logoImg from "@assets/logo.png";
 
-export function Header() {
+export type HeaderProps = {
+  showBackIcon?: boolean;
+};
+
+export function Header({ showBackIcon = false }: HeaderProps) {
   return (
     <Container>
+      {showBackIcon && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logoImg} />
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,4 @@
+import { CaretLeft } from "phosphor-react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View(({ theme }) => ({
@@ -5,9 +6,19 @@ export const Container = styled.View(({ theme }) => ({
   flexDirection: "row",
   justifyContent: "center",
   alignItems: "center",
+  marginTop: 32,
 }));
 
 export const Logo = styled.Image(() => ({
   height: 55,
   width: 46,
 }));
+
+export const BackButton = styled.TouchableOpacity(() => ({
+  flex: 1,
+}));
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 36,
+  color: theme.COLORS.WHITE,
+}))(() => ({}));
